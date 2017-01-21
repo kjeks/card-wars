ce1e26d159186e1e145de2ace831f58046a1edde
kjeks <danskejon@hotmail.com>
2017-01-21 19:11:57 +0100

    first commit

---
+++ Assets/DropZone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+	public draggable.Slot viablePlacement = draggable.Slot.MINION_FIELD;
+
+	public void OnDrop(PointerEventData eventData) {
+		Debug.Log (eventData.pointerDrag.name + " dropped onto " + gameObject.name);
+
+		var currentDragged = eventData.pointerDrag.GetComponent<draggable> ();
+		if(currentDragged != null) {
+			if (viablePlacement == currentDragged.viablePlacement) {
+				currentDragged.parentToReturnTo = this.transform;
+			}
+
+		}
+	}
+	public void OnPointerEnter(PointerEventData eventData) {
+		//Debug.Log ("onPointerEnter");
+	}
+	public void OnPointerExit(PointerEventData eventData) {
+		//Debug.Log ("onPointerExit");
+	}
+}
---
+++ Assets/draggable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public Transform parentToReturnTo = null;
+
+	public enum Slot {MINION_FIELD, HAND};
+	public Slot viablePlacement = Slot.HAND; 
+
+	public void OnBeginDrag(PointerEventData eventData) {
+		Debug.Log ("test");
+		parentToReturnTo = this.transform.parent;
+		this.transform.SetParent (this.transform.parent.parent);
+
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+	}
+	public void OnDrag(PointerEventData eventData) {
+		//Debug.Log ("dragging");
+
+		this.transform.position = eventData.position;
+	}
+	public void OnEndDrag(PointerEventData eventData) {
+		//Debug.Log ("end drag");
+		this.transform.SetParent (parentToReturnTo);
+		GetComponent<CanvasGroup> ().blocksRaycasts = true;
+	}
+}
