2b612b7ab013a2e9b34ecff28b52e1362772f27e
kjeks <danskejon@hotmail.com>
2017-01-29 20:29:25 +0100

    create basic functionality for card instances

--- Assets/CardCollection.cs
+++ Assets/CardCollection.cs
@@ -8,10 +8,14 @@ public class CardCollection : MonoBehaviour {
 	void Start () {
 		collection = new List<KeyValuePair <int, string []>> ();
 		collection.Add(new KeyValuePair<int, string []>(1, new string[6]{"scout", "this is a scout", "2", "2", "2", "red"}));
-		collection.Add(new KeyValuePair<int, string []>(2, new string[6]{"warrior", "this is a warrior", "2", "3", "3", "red"}));
-		collection.Add(new KeyValuePair<int, string []>(3, new string[6]{"big scout", "this is a big scout", "6", "4", "9", "red"}));
-		collection.Add(new KeyValuePair<int, string []>(4, new string[6]{"big warrior", "this is a big warrior", "6", "7", "9", "red"}));
-//			{2, new string ["warrior", "warrior", 5, 5, 5, "red"] }
+		collection.Add(new KeyValuePair<int, string []>(2, new string[6]{"warrior", "this is a warrior", "2", "3", "3", "blue"}));
+		collection.Add(new KeyValuePair<int, string []>(3, new string[6]{"big scout", "this is a big scout", "6", "4", "9", "green"}));
+		collection.Add(new KeyValuePair<int, string []>(4, new string[6]{"big warrior", "this is a big warrior", "6", "7", "9", "black"}));
+		collection.Add(new KeyValuePair<int, string []>(4, new string[6]{"fire imp", "", "1", "2", "1", "red"}));
+		collection.Add(new KeyValuePair<int, string []>(4, new string[6]{"frog", "", "2", "2", "3", "blue"}));
+		collection.Add(new KeyValuePair<int, string []>(4, new string[6]{"vulcano giant", "", "9", "9", "9", "red"}));
+		collection.Add(new KeyValuePair<int, string []>(4, new string[6]{"elf warrior", "", "2", "2", "3", "green"}));
+		collection.Add(new KeyValuePair<int, string []>(4, new string[6]{"elven healer", "", "3", "3", "5", "green"}));
 
 	}
 	public static string [] getCardValues (int cardId) {
@@ -19,8 +23,7 @@ public class CardCollection : MonoBehaviour {
 		return collection[cardId].Value;
 	}
 
-
-//	public string [] getCardValues (int cardId) {
-//		return collection [cardId];
-//	}
+	public static int getCollectionSize () {
+		return collection.Count;
+	}
 }
--- Assets/Deck.cs
+++ Assets/Deck.cs
@@ -14,8 +14,7 @@ public class Deck : MonoBehaviour {
 		handGO = GameObject.Find ("Hand");
 		hand = (Hand) handGO.GetComponent(typeof(Hand));
 
-		//populateDeckWithCards ();
-		addCardFromCollection();
+		populateDeckWithCards ();
 	
 	}
 	public Card generateCard (string title, string text, int cost, int attack, int health, Color color) {
@@ -32,43 +31,37 @@ public class Deck : MonoBehaviour {
 		hand.addCardToHand (newCard);
 	}
 		
-//	private void populateDeckWithCards () {
-//		for(int a=0; a<10; a++) {
-//			Card newCard = generateCard ();
-//			orderedDeck.Add (newCard);
-//		}
-//	}
-	public void addCardFromCollection () {
-		for (int a = 0; a < 3; a++) {
-			string [] values = CardCollection.getCardValues(a);
-			string title = values[0];
-			string text = values[1];
-			int attack = int.Parse(values[2]);
-			int health = int.Parse(values[3]) ;
-			int cost = int.Parse(values[4]); 
+	public void addRandomCardFromCollection () {
+		int randomCardId = (int)(Random.value * CardCollection.getCollectionSize ());
+		string [] values = CardCollection.getCardValues(randomCardId);
+		string title = values[0];
+		string text = values[1];
+		int attack = int.Parse(values[2]);
+		int health = int.Parse(values[3]) ;
+		int cost = int.Parse(values[4]); 
+		Color color = GetCorrectColor(values[5]);
 
-
-			Color color = GetCorrectColor(values[5]);
-			foreach (string value in values) {
-				orderedDeck.Add(generateCard (title, text, cost, attack, health, color));
-			}
-		}
+		orderedDeck.Add(generateCard (title, text, cost, attack, health, color));
 	}
+
 	public Color GetCorrectColor (string colorName){
 		switch (colorName) {
 		case "red":
 			return Color.red;
-			break;
 		case "blue":
 			return Color.blue;
-			break;
 		case "green":
 			return Color.green;
-			break;
 		default:
 			return Color.black;
 		}
 	}
+	public void populateDeckWithCards () {
+		while (orderedDeck.Count < 30) {
+			addRandomCardFromCollection ();
+		}
+	}
+
 
 	public Card drawCardFromDeck () {
 		Card drawnCard = orderedDeck [0];
